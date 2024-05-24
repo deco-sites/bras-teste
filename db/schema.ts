5940174634b5c5dbf14b7c7f88544033a43a2764
Igor Brasileiro <igor@deco.cx>
2024-05-24 13:02:12 +0000

    Release staging environment with 5 changes made by Igor Brasileiro

---
+++ db/schema.ts
@@ -0,0 +1,14 @@
+
+/**
+* The code snippet below is an example.
+*/
+
+/**
+* import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const profiles = sqliteTable("profiles", {
+  id: integer("col1").primaryKey({ autoIncrement: true }),
+  name: text("name"),
+  email: text("email"),
+});
+*/
