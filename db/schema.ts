f1483377526e644e3b96b9a5cf910a3db93ac36d
igorbrasileiro <brasileiro456@gmail.com>
2024-05-27 17:37:54 -0300

    Drop setup

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,13 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+/**
+ * The code snippet below is an example.
+ */
+
+/**
+* import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const profiles = sqliteTable("profiles", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name"),
   email: text("email"),
-  phone: text("phone"),
 });
+*/
--- drizzle/0000_warm_penance.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `profiles` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text,
-	`email` text
-);
