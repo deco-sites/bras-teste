6eb04aef51e356002a85765503317a21c1cd44e4
igorbrasileiro <brasileiro456@gmail.com>
2024-09-12 13:53:36 -0300

    Drop records and bump deps

--- db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const profiles = sqliteTable("profiles", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name"),
-  email: text("email"),
-});
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,35 +4,41 @@
 
 import * as $$$$$$$$$$$0 from "./apps/deco/records.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$0 from "./loaders/xpto.ts";
 import * as $$$$$$0 from "./sections/BlogPosts.tsx";
 import * as $$$$$$1 from "./sections/Faq.tsx";
 import * as $$$$$$2 from "./sections/Footer.tsx";
 import * as $$$$$$3 from "./sections/Header.tsx";
-import * as $$$$$$10 from "./sections/hero-1.tsx";
-import * as $$$$$$11 from "./sections/hero-2.tsx";
+import * as $$$$$$11 from "./sections/hero-1.tsx";
+import * as $$$$$$12 from "./sections/hero-2.tsx";
 import * as $$$$$$4 from "./sections/Hero.tsx";
-import * as $$$$$$12 from "./sections/hero2.tsx";
+import * as $$$$$$13 from "./sections/hero2.tsx";
 import * as $$$$$$5 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$6 from "./sections/ListUsers.tsx";
 import * as $$$$$$7 from "./sections/Logos.tsx";
 import * as $$$$$$8 from "./sections/Testimonials.tsx";
 import * as $$$$$$9 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$10 from "./sections/Xurebinha.tsx";
 
 const manifest = {
+  "loaders": {
+    "site/loaders/xpto.ts": $$$0,
+  },
   "sections": {
     "site/sections/BlogPosts.tsx": $$$$$$0,
     "site/sections/Faq.tsx": $$$$$$1,
     "site/sections/Footer.tsx": $$$$$$2,
     "site/sections/Header.tsx": $$$$$$3,
-    "site/sections/hero-1.tsx": $$$$$$10,
-    "site/sections/hero-2.tsx": $$$$$$11,
+    "site/sections/hero-1.tsx": $$$$$$11,
+    "site/sections/hero-2.tsx": $$$$$$12,
     "site/sections/Hero.tsx": $$$$$$4,
-    "site/sections/hero2.tsx": $$$$$$12,
+    "site/sections/hero2.tsx": $$$$$$13,
     "site/sections/ImageWithParagraph.tsx": $$$$$$5,
     "site/sections/ListUsers.tsx": $$$$$$6,
     "site/sections/Logos.tsx": $$$$$$7,
     "site/sections/Testimonials.tsx": $$$$$$8,
     "site/sections/Theme/Theme.tsx": $$$$$$9,
+    "site/sections/Xurebinha.tsx": $$$$$$10,
   },
   "apps": {
     "site/apps/deco/records.ts": $$$$$$$$$$$0,
